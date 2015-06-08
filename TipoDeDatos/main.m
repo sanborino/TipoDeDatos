d35836b9c5cd287d63c6d0a8473bccbd813431bf
sanborino <Ricardo@MacBook-Pro-de-Ricardo.local>
2015-06-08 15:50:32 -0600

    Initial Commit

---
+++ TipoDeDatos/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TipoDeDatos
+//
+//  Created by Ricardo A Mondragon A on 08/06/15.
+//  Copyright (c) 2015 Ricardo A Mondragon A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
